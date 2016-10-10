3c60b3b3bfafd19b49ccda9cb3bfda3be0630c75
Lauren Hanlon <laurenmhanlon@gmail.com>
2016-10-10 10:07:35 -0700

    allows for missing values in range function

--- test-that/functions/functions.R
+++ test-that/functions/functions.R
@@ -1,6 +1,8 @@
 #' @title Range Value
 #' @param x numeric vector
 #' @return range as max - min
+
+#na.rm set automatically to FALSE, change to TRUE if missing values exist
 range_value <- function(x, na.rm=FALSE) {
   if (na.rm){
     new_x <- na.omit(x)
